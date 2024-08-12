ae1bc3183109b347a1d66dbd5efc1c953d8a3553
Liheng Cao <20694415+lihengcao@users.noreply.github.com>
2024-08-12 12:21:27 -0400

    separate dev and user setup

--- setup_dev.sh
+++ setup_dev.sh
@@ -2,6 +2,6 @@ python3 -m venv .venv
 source .venv/bin/activate
 printf "\nPackages Before:\n"
 pip list
-pip install -r static-site-generator/requirements.txt
+pip install -r requirements.txt
 printf "\nPackages After:\n"
 pip list
